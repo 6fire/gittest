71a86cafedbf7aa58e5e22741ef9aeb4af6722c9
Administrator <fd@qq.com>
2017-06-09 10:19:46 +0800

    45

--- gittest/src/test/Ta.java
+++ gittest/src/test/Ta.java
@@ -2,7 +2,7 @@ package test;
 
 public class Ta {
     public int sum(int x,int y){return x+y;}
-    public void print(){}
+    public void print(int x){}
 	public static void main(String[] args) {
 		 
 
