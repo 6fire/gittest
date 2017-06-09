792fc5ccbb8568c3f8c2d198c60a233493f80790
Administrator <fd@qq.com>
2017-06-09 12:08:20 +0800

    删除sum

--- gittest/src/gittest/Ta.java
+++ gittest/src/gittest/Ta.java
@@ -2,7 +2,7 @@ package gittest;
 
 public class Ta {
     public void print(int x){}
-    public int sum(int x,int y){return x+y;}
+ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
